37ce32956c4c79f7215f5538d0c37c451ec666cb
Victor <victorpa.iut@gmail.com>
2016-04-09 13:31:59 +0200

    Premier Commit

---
+++ src/doors/App.java
@@ -0,0 +1,40 @@
+package doors;
+
+import sun.applet.Main;
+
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+import java.io.*;
+import java.awt.GraphicsEnvironment;
+import java.net.URISyntaxException;
+
+/**
+ * Created by Victor on 08/04/2016.
+ * @author Brandon Barajas
+ */
+public class App {
+    public static void main(String[] args) throws IOException {
+
+        new Game();
+        Scanner scanner = new Scanner(System.in);
+        int rejouer;
+        while (true) {
+            try {
+                System.out.println("Relancer une simulation?");
+                System.out.println("Chiffre: Oui");
+                System.out.println("Autre: Terminer");
+                rejouer = Integer.parseInt(scanner.nextLine());
+                new Game();
+
+            } catch (NumberFormatException e) {
+                System.out.println("Fin de la simulation");
+                break;
+
+            }
+
+        }
+
+
+    }
+}
